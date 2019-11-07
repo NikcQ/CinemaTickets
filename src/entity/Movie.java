d4442b82cad8d6c9d4ad9d1084a2673ac0c334ed
user <user@10.203.142.218>
2019-11-07 15:08:29 -0500

    Movie Entity

---
+++ src/entity/Movie.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author user
+ */
+public class Movie {
+    private String title;
+    private String [] genre;
+    private String [] languages;
+    private String rating;
+    private String cast;
+    private LocalDate date;
+}
