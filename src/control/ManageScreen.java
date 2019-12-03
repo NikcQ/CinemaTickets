6d90961d6f16beffb07d725b6c56403f55bb1249
NikcQ <>
2019-12-02 21:10:05 -0500

    Screen Control class finished (give it a look)

--- src/control/ManageScreen.java
+++ src/control/ManageScreen.java
@@ -6,7 +6,11 @@ package control;
  */
 import DAO.ScreenDAO;
 import boundary.mainFrame;
-import entity.*;
+import entity.Screen.*; 
+import entity.CinemApp;
+import entity.Screen;
+import static entity.Screen_.row4DX;
+import entity.Theater;
 import java.util.ArrayList;
 
 
@@ -20,25 +24,60 @@ public class ManageScreen {
     
     public String verificarScreen(int rowGA, int colGA, int rowVIP, int colVIP, int row4DX, int col4DX, boolean is3D, String name){
     
-    Screen nueva=new Screen();
-    nueva.setRowGA(rowGA);
-    nueva.setColGA(colGA);
-    nueva.setRowVIP(rowVIP);
-    nueva.setColVIP(colVIP);
-    nueva.setRow4DX(row4DX);
-    nueva.setCol4DX(col4DX);
-    nueva.setIs3D(is3D);
-    nueva.setName(name);
-
-    
-
-    
-    teatro.addScreen(nueva);
-    salas.create(nueva);
+        if (TestScreenRows(rowGA, rowVIP, row4DX)){
+            
+            return ("error 1");
+        }
+        
+        if (TestScreenColumns (colGA, colVIP, col4DX)){
+        
+        return("error 2");
+        
+        }
+        if (TestScreenName(name)){
+            
+            return ("error 3");
+            
+        }
+            
+    Screen s = new Screen();
+    s.setRowGA(rowGA);
+    s.setColGA(colGA);
+    s.setRowVIP(rowVIP);
+    s.setColVIP(colVIP);
+    s.setRow4DX(row4DX);
+    s.setCol4DX(col4DX);
+    s.setIs3D(is3D);
+    s.setName(name);
+   
+    teatro.addScreen(s);
+    salas.create(s);
         printScreen();
     return ("Registro Correcto");
 
     }
+    
+    public boolean TestScreenRows (int rowGA, int rowVIP, int row4DX){
+    
+        return (rowGA < 0 || rowVIP < 0 || row4DX < 0);
+        
+        
+    } 
+      
+    public boolean TestScreenColumns (int colGA, int colVIP, int col4DX){
+    
+    return (colGA < 0 || colVIP < 0 || col4DX < 0);
+    }
+    
+    public boolean TestScreenName (String name){
+    boolean istrue = false;
+    if (name.isEmpty()){
+        istrue = true;
+    }
+        return istrue;
+    }
+    
+    
     
              public void printScreen(){
          for (Screen screen : teatro.getScreens()) {
@@ -48,15 +87,7 @@ public class ManageScreen {
 
      }
             
-
-
-    
-   
-    
-          
-    
-    
-    
+  
     public static ArrayList<String> getScreenNames(){
         ArrayList<Screen> list = mainFrame.cinemapp.getCinema().getScreens();
         ArrayList<String> names = new ArrayList<String>();
