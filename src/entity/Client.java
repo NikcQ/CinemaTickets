84a7e97b22715a270273776f64bf5e2a71b9f70a
NikcQ <NikcQ@NikcQ>
2020-01-20 16:31:11 -0500

    Added Client Entity

---
+++ src/entity/Client.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author NikcQ
+ */
+public class Client {
+    
+    private String Fname;
+    private String Lname;
+    private String birthDate;
+    private int id;
+    private int scoreBalance;
+    private int phone;
+    private String email;
+    //private boolean activity;
+    
+    public Client(){
+    }
+
+    public String getFname() {
+        return Fname;
+    }
+
+    public void setFname(String Fname) {
+        this.Fname = Fname;
+    }
+
+    public String getLname() {
+        return Lname;
+    }
+
+    public void setLname(String Lname) {
+        this.Lname = Lname;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getScoreBalance() {
+        return scoreBalance;
+    }
+
+    public void setScoreBalance(int scoreBalance) {
+        this.scoreBalance = scoreBalance;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+    
+}
